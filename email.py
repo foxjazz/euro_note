268a4c6d0c1e425631b7449b5aaeab1c022d44d6
foxjazz <foxr4@foxjazz.net>
2024-01-06 11:18:35 -0700

    updated for script ssmtp mail

--- email.py
+++ email.py
@@ -1,10 +1,38 @@
-import smtplib
-from email.mime.text import MIMEText
-msg = MIMEText("EUR went up")
-msg['Subject'] = "EUR went up"
-msg['From'] = "joe@foxjazz.net"
-msg['To'] = "mail@foxjazz.net"
-server = smtplib.SMTP('smtp.ionos.com', 587)  # Replace with your email server's details
-server.starttls()
-server.login("send@foxjazz.net", "TheStruggle88**99((00))")  # Replace with your email credentials
-server.sendmail(msg['From'], msg['To'], msg.as_string())
+import os.path
+import time
+import platform
+import subprocess
+def sendMail(some):
+    if os_name == "Darwin":
+        with open("pw.txt", 'r') as file:
+            pw = file.readline()
+        msg = MIMEText("EUR went up")
+        msg['Subject'] = "EUR went up " + some
+        msg['From'] = "joe@foxjazz.net"
+        msg['To'] = "mail@foxjazz.net"
+        server = smtplib.SMTP('smtp.ionos.com', 587)  # Replace with your email server's details
+        server.starttls()
+        server.login("send@foxjazz.net", pw)  # Replace with your email credentials
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+    else:
+        file_path = 'mail.txt'  # Replace with the path to your file
+
+# Open the file in write mode ('w')
+        with open(file_path, 'w') as file:
+            # Write multiple lines of text
+            file.write("From: send@foxjazz.net\n")
+            file.write("Subject: EUR is {some} up.\n")
+            file.write("Euro is up by {some}\n")
+        command = '"ssmtp joe@foxjazz.net < mail.txt"'
+        try:
+            # Run the command and capture the output
+            output = subprocess.check_output(command, shell=True, text=True)
+            
+            # Print the output
+            print("Command Output:")
+            print(output)
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing the command: {e}")
+            
+sendMail(".005")            
+
--- euquote.py
+++ euquote.py
@@ -4,6 +4,11 @@ import smtplib
 from email.mime.text import MIMEText
 import os.path
 import time
+import platform
+import subprocess
+
+# Get the operating system name and version
+os_name = platform.system()
 
 def getEURUSD():
     conn = http.client.HTTPSConnection("alpha-vantage.p.rapidapi.com")
@@ -22,19 +27,39 @@ def getEURUSD():
     return json.loads(dc)
     
 def sendMail(some):
-    with open("pw.txt", 'r') as file:
-        pw = file.readline()
-    msg = MIMEText("EUR went up")
-    msg['Subject'] = "EUR went up " + some
-    msg['From'] = "joe@foxjazz.net"
-    msg['To'] = "mail@foxjazz.net"
-    server = smtplib.SMTP('smtp.ionos.com', 587)  # Replace with your email server's details
-    server.starttls()
-    server.login("send@foxjazz.net", pw)  # Replace with your email credentials
-    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    if os_name == "Darwin":
+        with open("pw.txt", 'r') as file:
+            pw = file.readline()
+        msg = MIMEText("EUR went up")
+        msg['Subject'] = "EUR went up " + some
+        msg['From'] = "joe@foxjazz.net"
+        msg['To'] = "mail@foxjazz.net"
+        server = smtplib.SMTP('smtp.ionos.com', 587)  # Replace with your email server's details
+        server.starttls()
+        server.login("send@foxjazz.net", pw)  # Replace with your email credentials
+        server.sendmail(msg['From'], msg['To'], msg.as_string())
+    else:
+        file_path = 'mail.txt'  # Replace with the path to your file
+
+# Open the file in write mode ('w')
+        with open(file_path, 'w') as file:
+            # Write multiple lines of text
+            file.write("From: send@foxjazz.net\n")
+            file.write("Subject: EUR is {some} up.\n")
+            file.write("Euro is up by {some}\n")
+        command = '"ssmtp joe@foxjazz.net < mail.txt"'
+        try:
+            # Run the command and capture the output
+            output = subprocess.check_output(command, shell=True, text=True)
+            
+            # Print the output
+            print("Command Output:")
+            print(output)
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing the command: {e}")
 
 
-quote = 1.1
+quote = .7
 fe = True
 file_path = 'last_quote.json'
 if not os.path.exists(file_path):
